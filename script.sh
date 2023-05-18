849aa35051166f1fb8b8fdec90b7f43f5083be60
Kosty Manuhin <kostymanuhin@gmail.com>
2023-05-18 18:36:30 +0300

    deprecated script -> not deprecated

--- script.sh
+++ script.sh
@@ -2,13 +2,13 @@
 
 servers=$1
 
-gnome-terminal --tab --title="Gateway" -e "java -jar gateway/build/libs/gateway-1.0-SNAPSHOT.jar" &
+gnome-terminal --tab --title="Gateway" -- java -jar gateway/build/libs/gateway-1.0-SNAPSHOT.jar &
 sleep 1
 
 for ((i=0; i<servers; i++)); do
-  gnome-terminal --tab --title="Server $i" -e "java -jar server/build/libs/server-1.0-SNAPSHOT.jar" &
+  gnome-terminal --tab --title="Server $i" -- java -jar server/build/libs/server-1.0-SNAPSHOT.jar &
   sleep 1
 done
 
 sleep 1
-gnome-terminal --tab --title="Client" -e "java -jar client/build/libs/client-1.0-SNAPSHOT.jar" &
+gnome-terminal --tab --title="Client" -- java -jar client/build/libs/client-1.0-SNAPSHOT.jar &
